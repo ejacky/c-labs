b6ca325817268c637c85b335c643b9cb8197c555
admin <test@qq.com>
2017-05-22 08:36:57 +0800

    add

---
+++ swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void swap(int *pa, int *pb);
+
+int main(void)
+{
+	int a = 5;
+	int b = 6;
+	swap(&a, &b);
+	printf("a=%d, b=%d\n", a, b);
+}
+
+void swap(int *pa, int *pb)
+{
+	int t = *pa;
+	*pa = *pb;
+	*pb = t;
+}
