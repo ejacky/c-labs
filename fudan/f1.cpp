836d99e3dbb78a195425d4c2046c4c471da957d0
zhangjianfei <zhangjianfei@b.360.cn>
2017-11-10 14:54:34 +0800

    modify

--- fudan/f1.cpp
+++ fudan/f1.cpp
@@ -35,7 +35,7 @@ void e4()
 			if (max < d[j]) 
 				max = d[j];
 		} 
-		printf("max:%d", max);
+		printf("%d", max);
 	} else {
 		printf("数组个数不对，目前有:%d 个", n);
 	}
@@ -43,17 +43,73 @@ void e4()
 
 void e5()
 {
-	int d[100];
-	while () {
-	}	
+	char d[100];
+	scanf("%s", d);
+	int i = 0;
+	int count = 0;
+	while(d[i] != '\0') {
+		if (d[i] >= 'A' && d[i] <= 'Z')
+			count++;
+		i++;
+	}
+	printf("%d", count);
 } 
 
+void e7()
+{
+	struct student {
+		char no[8];
+		int score1;
+		int score2;
+		int score3;
+	};
+	struct student s[3];
+	for (int i = 0; i < 3; i++) {
+		scanf("%s %d %d %d", s[i].no, &(s[i].score1), &(s[i].score2), &(s[i].score3));
+	}
+	for (int j = 0; j < 3; j++) {
+		printf("%s %d\n", s[j].no, (s[j].score1 + s[j].score2 + s[j].score3));
+	}
+}
+
+void e8()
+{
+	int *d[3][4];
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 4; j++)
+			scanf("%d", *(d[i]+j);
+			
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 4; j++)
+			printf("%d", d[i]+j;
+		printf("\r\n");
+			
+}
+
+int count(int a[], int n, int x)
+{
+	int c = 0;
+	for(int i = 0; i < n; i++) 
+		if (a[i] == x)
+			c++;
+	return c;
+}
 int main()
 {
 	//e1();
 	//e3();
-	e4();
-
+	//e4();
+	//e5();
+	//e7();
+	e8();
+	
+//	int d[15];
+//	int y;
+//	for (int i = 0; i < 15; i++) {
+//		scanf("%d", &d[i]);
+//	}
+//	scanf("%d", &y);
+//	printf("%d", count(d, 15, y));
 }
 
 
